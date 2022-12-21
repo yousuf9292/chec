7d56d2ab25785fc62f7c81a7f4d5a31593595ac4
Syed Yousuf Naqvi <43644200+yousuf9292@users.noreply.github.com>
2022-12-21 20:29:40 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ selected_file = lista_wb[lista_wb_without_ext.index(selected_workbook)]
 print(selected_file)
 
 # Construct the full path to the file
-file_path = 'Index/GP Nr. 05,06,08.xlsx'
+file_path = 'https://github.com/yousuf9292/chec/blob/main/Index/GP%20Nr.%2005%2C06%2C08.xlsx'
 
 # Open the Excel file using the pandas ExcelFile class
 excel_file = pd.ExcelFile(file_path)
