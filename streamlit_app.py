68077658f4581017f6c4a5056ad74432803e9c51
Syed Yousuf Naqvi <43644200+yousuf9292@users.noreply.github.com>
2022-12-21 19:50:31 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import numpy as np
 from PIL import Image
 
 # Change the current working directory to H:\Python\Index
-os.chdir("Index/GP Nr. 05,06,08.xlsx")
+os.chdir("Index/")
 
 # Get the current directory
 current_dir = os.getcwd()
